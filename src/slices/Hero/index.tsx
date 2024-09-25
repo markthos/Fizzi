8cc9f7485cf500d2a8f64fc7fd9882a62bf10d2a
markthos <aydenthos@gmail.com>
2024-09-25 15:50:54 -0700

    added scroll trigger to hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,13 +5,14 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
 
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 /**
  * Props for `Hero`.
  */
@@ -42,16 +43,41 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     .from(".hero-body", {
       y: 10,
       opacity: 0,
-    },
-    )
+    })
     .from(".hero-button", {
       y: 10,
       opacity: 0,
       duration: 0.6,
-    },
+    })
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+    scrollTl
+      .fromTo("body", { 
+        backgroundColor: "#FDE047"
+
+      }, { 
+        backgroundColor: "#D9F99D",
+        overwrite: "auto"
+      },
+      1,
     )
-  }
-  );
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: .1,
+        ease: "back.out(3)",
+        duration: .5
+    });
+  });
 
   return (
     <Bounded
@@ -78,19 +104,20 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               <Button buttonLink={slice.primary.button_link} buttonText={slice.primary.button_text} className="hero-button mt-12"/>
             </div>
           </div>
-          <div className="grid text-side relative z-[80] h-screen items-center gap-4 md:grid-cols-2">
-            <PrismicNextImage 
-              className="md:hidden w-full"
-              field={slice.primary.cans_image} />
-              <div className="">
-                <h2 className="text-side-heading text-balance text-6xl font-black uppercase text-sky-950 lg:text-8xl">
-                  <TextSplitter text={asText(slice.primary.second_heading)} />
-                  </h2>
-                  <div className="text-side-heading mt-4 max-w-xl text-balance text-xl font-normal text-sky-950">
-                    <TextSplitter text={asText(slice.primary.second_body)} />
-                  </div>
-              </div>
+          <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols-2">
+          <PrismicNextImage
+            className="w-full md:hidden"
+            field={slice.primary.cans_image}
+          />
+          <div>
+            <h2 className="text-side-heading text-balance text-6xl font-black uppercase text-sky-950 lg:text-8xl">
+              <TextSplitter text={asText(slice.primary.second_heading)} />
+            </h2>
+            <div className="text-side-body mt-4 max-w-xl text-balance text-xl font-normal text-sky-950">
+              <PrismicRichText field={slice.primary.second_body} />
+            </div>
           </div>
+        </div>
       </div>
     </Bounded>
   );
