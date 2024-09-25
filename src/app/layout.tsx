c3a3bc4c094afb1b4da785aa8e83a99dc7477351
markthos <aydenthos@gmail.com>
2024-09-25 09:26:16 -0700

    added header logo and animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
+import localFont from 'next/font/local'
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+
 import "./app.css";
+import Header from '@/components/Header';
+
+const alpino = localFont({
+  src: '../../public/fonts/Alpino-Variable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: "--font-alpino",
+})
 
 export default function RootLayout({
   children,
@@ -8,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={alpino.variable}>
+      <body>
+        <Header/>
+        <main>{children}</main>
+        </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
