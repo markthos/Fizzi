18b1f9666df935a958d099018130329e4f1868b0
markthos <aydenthos@gmail.com>
2024-10-06 09:32:21 -0700

    added threejs modeling

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { Environment } from "@react-three/drei";
-import { use, useRef } from "react";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
 import { Group } from "three";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import FloatingCan from "@/components/FloatingCan";
-import { register } from "module";
 
-gsap.registerPlugin(useGSAP);
+
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 type Props = {}
 
@@ -49,8 +51,63 @@ export default function Scene({}: Props) {
       gsap.set(can3Ref.current.position, { y: 5, z: 2 });
       gsap.set(can4Ref.current.position, { x: 2, y: 4, z: 2 });
       gsap.set(can5Ref.current.position, { y: -5 });
-    })
 
+      const introTl = gsap.timeline({
+        defaults: { 
+          duration: 3, 
+          ease: "back.out(1.4)" },
+      })
+
+      if (window.scrollY < 20) {
+        introTl
+          .from(can1GroupRef.current.position, { y: -5, x: 1 }, 0)
+          .from(can1GroupRef.current.rotation, { z: 3 }, 0)
+          .from(can2GroupRef.current.position, { y: 5, x: 1 }, 0)
+          .from(can2GroupRef.current.rotation, { z: 3 }, 0);
+      }
+
+      const scrollTl = gsap.timeline({
+        defaults: {
+          duration: 2,
+        },
+        scrollTrigger: {
+          trigger: ".hero",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 1.5,
+          markers: true,
+        },
+      });
+
+      scrollTl
+      // Rotate can group
+      .to(groupRef.current.rotation, { y: Math.PI * 2 })
+
+      // Can 1 - black cherry
+      .to(can1Ref.current.position, { x: -0.2, y: -0.7, z: -2 }, 0)
+      .to(can1Ref.current.rotation, { z: 0.3 }, 0)
+
+      // Can 2 - Lemon Lime
+      .to(can2Ref.current.position, { x: 1, y: -0.2, z: -1 }, 0)
+      .to(can2Ref.current.rotation, { z: 0 }, 0)
+
+      // Can 3 - Grape
+      .to(can3Ref.current.position, { x: -0.3, y: 0.5, z: -1 }, 0)
+      .to(can3Ref.current.rotation, { z: -0.1 }, 0)
+
+      // Can 4 - Strawberry Lemonade
+      .to(can4Ref.current.position, { x: 0, y: -0.3, z: 0.5 }, 0)
+      .to(can4Ref.current.rotation, { z: 0.3 }, 0)
+
+      // Can 5 -Watermelon
+      .to(can5Ref.current.position, { x: 0.3, y: 0.5, z: -0.5 }, 0)
+      .to(can5Ref.current.rotation, { z: -0.25 }, 0)
+      .to(
+        groupRef.current.position,
+        { x: 1, duration: 3, ease: "sine.inOut" },
+        1.3,
+      );
+  });
 
   return (
     <group ref={groupRef}>
@@ -62,13 +119,14 @@ export default function Scene({}: Props) {
         <FloatingCan ref={can2Ref} flavor="lemonLime" floatSpeed = {FLOAT_SPEED} />
       </group>
 
-    <FloatingCan ref={can3Ref} flavor="grape" floatSpeed = {FLOAT_SPEED} />
+      <FloatingCan ref={can3Ref} flavor="grape" floatSpeed = {FLOAT_SPEED} />
 
-    <FloatingCan ref={can4Ref} flavor="strawberryLemonade" floatSpeed = {FLOAT_SPEED} />
+      <FloatingCan ref={can4Ref} flavor="strawberryLemonade" floatSpeed = {FLOAT_SPEED} />
 
-    <FloatingCan ref={can5Ref} flavor="watermelon" floatSpeed = {FLOAT_SPEED} />
+      <FloatingCan ref={can5Ref} flavor="watermelon" floatSpeed = {FLOAT_SPEED} />
 
-        <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5} />
+      <OrbitControls />
+      <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5} />
     </group>
   )
 }
